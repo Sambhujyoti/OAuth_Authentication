4673697509c1e1dc9c1c94bfc38b1eaaf7bee2b4
Sambhujyoti Das <sambhujyotidas@Sambhujyotis-MacBook-Air.local>
2023-06-03 12:06:00 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(() => {
+
+    const userSchema = new mongoose.Schema({
+        email: String,
+        password: String
+    });
+
+    const User = new mongoose.model("User", userSchema);
+
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
+
+    app.get("/register", (req, res) => {
+        res.render("register");
+    });
+
+    app.get("/login", (req, res) => {
+        res.render("login");
+    });
+
+    app.post("/register", (req, res) => {
+        const newUser = new User ({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save().then(() =>{
+            res.render("secrets");
+        }).catch((err) => {
+            console.error(err);
+        });
+    });
+
+    app.post('/login', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: username}).then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                } else {
+                    console.log("Sorry! Incorrect Password.");
+                }
+            } else {
+                console.log("The userid does not exit!");
+            }
+        }).catch((err) => {
+            console.error(err);
+        });
+    });
+
+
+
+
+
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log("The server is running on port " + port);
+    });
+}).catch((err) => {
+    console.error(err);
+});
