91a644f914580e7275e17bb2d9a5d52afaf5d18d
Sambhujyoti Das <sambhujyotidas@Sambhujyotis-MacBook-Air.local>
2023-06-05 22:11:58 +0530

    Add higher authintication level

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption"); // Using Encription Method.
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -18,8 +21,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(() => {
         password: String
     });
 
-    const secret = process.env.SECRET_KEY;
-    userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+/////////////////////////// Using Encription Method ///////////////////////////
+
+    // const secret = process.env.SECRET_KEY;
+    // userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
     const User = new mongoose.model("User", userSchema);
 
@@ -36,39 +41,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(() => {
     });
 
     app.post("/register", (req, res) => {
-        const newUser = new User ({
-            email: req.body.username,
-            password: req.body.password
-        });
-        newUser.save().then(() =>{
-            res.render("secrets");
-        }).catch((err) => {
-            console.error(err);
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+            const newUser = new User ({
+                email: req.body.username,
+                password: hash             // password: md5(req.body.password)
+            });
+            newUser.save().then(() =>{
+                res.render("secrets");
+            }).catch((err) => {
+                console.error(err);
+            });
         });
     });
 
     app.post('/login', (req, res) => {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = req.body.password;         // password: md5(req.body.password);
 
         User.findOne({email: username}).then((foundUser) => {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
+                    bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if (result === true) {             // if (foundUser.password === password) {
+                        res.render("secrets");
+                    } else {
+                        console.log("Sorry! Incorrect Password.");
+                    }
+                });
                 } else {
-                    console.log("Sorry! Incorrect Password.");
+                    console.log("The userid does not exit!");
                 }
-            } else {
-                console.log("The userid does not exit!");
-            }
         }).catch((err) => {
             console.error(err);
         });
     });
-
-
-
-
 
 
     const port = process.env.PORT || 3000;
